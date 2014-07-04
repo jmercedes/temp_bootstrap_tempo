311b44418c97f12b15b2e5aea4b4b8f01d2ef5ef
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-07-04 09:06:34 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicaCanela::Application.config.secret_token = '228763e02b2f284ea93ddfef94de6602d0322c32ff7e9a25b193e79b6765b900d2eb9444a16d5ceff7bb0bf12245687f5f7d24cb6901801d3d1eb1b55943eae7'
