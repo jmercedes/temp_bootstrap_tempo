0aa89d1f588ff8a1702b1e412bd84c420feaf73d
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-07-05 13:28:23 -0400

    add admin assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,8 @@ ClinicaCanela::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # refinerycms admin assets
+  config.assets.precompile += ['refinery/refinery.css']
+  
 end
